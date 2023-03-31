874b86c076b1c08fd9bd5d4b568d625efb15f2e6
rommelcazenas <rommel.cazenas.dit.cvsu@gmail.com>
2023-03-31 10:47:33 +0800

    final fixed

--- src/App.jsx
+++ src/App.jsx
@@ -12,42 +12,56 @@ function App() {
   const nav = document.querySelector("#navlinks");
 
   const clickBurger = () => {
-    // if(nav.classList.contains('hidden')) {
-    //   nav.classList.remove('hidden');
-    // }
-    // else {
-    //   nav.classList.add('hidden');
-    // }
+    setToggleBurger(!toggleBurger);
+    // console.log(toggleBurger);
+    const list = document.querySelector('ul');
+    toggleBurger === false ? (list.classList.add('top-[120]'), list.classList.add('opacity-100')) : (list.classList.remove('top-[120]'), list.classList.remove('opacity-100'))
   }
 
   return (
     <>
       <div className="App">
 
-        {/* FIXED NAVIGATION BAR */}
-        <div className=" px-3 py-3 bg-secondaryColor shadow-xl fixed top-0 z-50 border-b border-subTextColor w-full md:px-16 md:py-2">
-          <div className="navbar flex justify-between items-center" id="navbar">
+
+
+
+
+
+      {/* FIXED NAVIGATION BAR */}
+      <div className="px-3 py-3 bg-secondaryColor shadow-xl fixed top-0 z-50 border-b border-subTextColor w-full md:px-16 md:py-2">
+          <div className="md:flex md:justify-between md:items-center md:py-2 md:px-16"> {/* NAVIGATION BAR START HERE */}
+            {/* NAVIGATION HEADER/LOGO HERE */}
             <div className="flex items-center justify-between">
               <span className="text-2xl uppercase font-bold tracking-widest cursor-pointer hover:text-primaryColor transition duration-150">
                 <a href="#home">Meng.</a>
-              </span>
-              
+              </span>          
+
+              {/* NAVIGATION BURGER HERE */}
+              <span className="md:hidden block cursor-pointer transition-all ease-in-out duration-300" onClick={clickBurger}>
+                  {
+                    toggleBurger === false ? <GiHamburgerMenu className="icon-btn" size={20}/> : <MdOutlineClose className="icon-btn" size={25}/>
+                  }       
+                </span>
             </div>
-            <div>           
-              <ul className="hidden sm:flex gap-5 items-center cursor-pointer" id="navlinks">
+
+            {/* NAVIGATION LINKS HERE */}          
+              <ul className="left-0 my-0 pt-2 pb-2 md:my-0 md:pb-0 z-[1] bg-secondaryColor border-b border-subTextColor md:border-none flex flex-col md:flex-row items-center gap-3 md:gap-5 md:z-auto md:static absolute w-full md:w-auto opacity-0 md:opacity-100 top-[-500] transition-all ease-in duration-400" onClick={clickBurger}> 
                 <li className="hover:text-primaryColor"><a href="#about" className="transition duration-150">About</a></li>
                 <li className="hover:text-primaryColor"><a href="#project" className="transition duration-150">Projects</a></li>
                 <li className="hover:text-primaryColor"><a href="#contact" className="transition duration-150">Contact</a></li>
                 <li><a href="https://drive.google.com/file/d/1FjgXq5pNMTDIgdS-vmTvrbLZRAHWSdC4/view?usp=sharing"><button className="btn hover:bg-opacity-95">Resume</button></a></li>
               </ul>
-            </div>
-            <span className="sm:hidden" onClick={clickBurger}>
-              {
-                toggleBurger === false ? <GiHamburgerMenu className="icon-btn" size={20}/> : <MdOutlineClose className="icon-btn" size={20}/>
-              }       
-            </span>
           </div>
-        </div>   
+        </div>
+
+
+
+
+
+
+
+
+           
         <div className="main-container">
           <section id="home">
             <Home />  
